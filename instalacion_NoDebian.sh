b4ec7ef1881ca883f5e67caae0bd4bb7151699d3
Alquitran <125880660+roberlks@users.noreply.github.com>
2024-09-24 13:31:06 +0200

    Add files via upload
    Script de isntalacion modificado para usar Pacman en vez de apt

---
+++ instalacion_NoDebian.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Verificar si Podman ya está instalado
+if command -v podman &>/dev/null; then
+    echo "Podman ya está instalado."
+else
+    # Actualizar el sistema
+    sudo pacman -Syu
+    
+    # Instalar Podman
+    sudo pacman -S --noconfirm podman
+    
+    echo "Podman se ha instalado correctamente."
+fi
+
+# Verificar si el archivo "$HOME/.config/containers/registries.conf" existe
+if [ -e "$HOME/.config/containers/registries.conf" ]; then
+    echo "El archivo \"$HOME/.config/containers/registries.conf\" ya existe. Debería contener la siguiente línea:"
+    echo "unqualified-search-registries = ['nvcr.io', 'docker.io', 'quai.io']"
+    echo "------------------------------------"
+else
+    # Crear directorio para la configuración de containers
+    mkdir -p "$HOME/.config/containers/"
+    
+    # Configurar registros no calificados
+    echo "unqualified-search-registries = ['docker.io', 'nvcr.io', 'quai.io']" > "$HOME/.config/containers/registries.conf"
+    
+    echo "Se ha creado el archivo \"$HOME/.config/containers/registries.conf\" con la configuración necesaria."
+    echo "------------------------------------"
+fi
+
+# Continuar con el proceso de construcción
+podman build -t ubuntu-so .
